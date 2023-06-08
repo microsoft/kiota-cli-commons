4fcfd6e3551f0ff327c3d80bb9f044e0e2d4f3fb
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2023-06-09 00:19:47 +0300

    Chore/fix missing formatter (#43)
    * Register the NONE output type with output formatter factory
    
    * Bump version
    
    * Add none output formatter validation

--- src/Microsoft.Kiota.Cli.Commons.Tests/IO/OutputFormatterFactoryTest.cs
+++ src/Microsoft.Kiota.Cli.Commons.Tests/IO/OutputFormatterFactoryTest.cs
@@ -9,18 +9,28 @@ public class OutputFormatterFactoryTest
     public class GetFormatterFunction_Should
     {
         [Theory]
-        [InlineData(FormatterType.NONE)]
-        public void ThrowException_On_Invalid_FormatterType(FormatterType formatterType)
+        [InlineData((FormatterType)20)]
+        public void ThrowException_On_Invalid_Enum_FormatterType(FormatterType formatterType)
         {
             var factory = new OutputFormatterFactory();
 
-            Assert.Throws<NotSupportedException>(() => factory.GetFormatter(formatterType));
+            Assert.Throws<ArgumentOutOfRangeException>(() => factory.GetFormatter(formatterType));
+        }
+
+        [Theory]
+        [InlineData("invalid")]
+        public void ThrowException_On_Invalid_String_FormatterType(string formatterType)
+        {
+            var factory = new OutputFormatterFactory();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => factory.GetFormatter(formatterType));
         }
 
         [Theory]
         [InlineData(FormatterType.JSON, typeof(JsonOutputFormatter))]
         [InlineData(FormatterType.TABLE, typeof(TableOutputFormatter))]
         [InlineData(FormatterType.TEXT, typeof(TextOutputFormatter))]
+        [InlineData(FormatterType.NONE, typeof(NoneOutputFormatter))]
         public void Return_OutputFormatter_On_FormatterType(FormatterType formatterType, Type expectedType)
         {
             var factory = new OutputFormatterFactory();
@@ -38,6 +48,8 @@ public class OutputFormatterFactoryTest
         [InlineData("TABLE", typeof(TableOutputFormatter))]
         [InlineData("text", typeof(TextOutputFormatter))]
         [InlineData("TEXT", typeof(TextOutputFormatter))]
+        [InlineData("none", typeof(NoneOutputFormatter))]
+        [InlineData("NONE", typeof(NoneOutputFormatter))]
         public void Return_OutputFormatter_On_FormatterType_String(string formatterType, Type expectedType)
         {
             var factory = new OutputFormatterFactory();
--- src/Microsoft.Kiota.Cli.Commons/IO/OutputFormatterFactory.cs
+++ src/Microsoft.Kiota.Cli.Commons/IO/OutputFormatterFactory.cs
@@ -1,10 +1,12 @@
-using System;
+﻿using System;
 
 namespace Microsoft.Kiota.Cli.Commons.IO;
 
 /// <inheritdoc />
 public sealed class OutputFormatterFactory : IOutputFormatterFactory
 {
+    private const string INVALID_FORMATTER_ERROR = "The formatter type specified is not valid. Ensure any new formatters are registered with the OutputFormatterFactory";
+
     /// <inheritdoc />
     public IOutputFormatter GetFormatter(FormatterType formatterType)
     {
@@ -13,7 +15,8 @@ public sealed class OutputFormatterFactory : IOutputFormatterFactory
             FormatterType.JSON => new JsonOutputFormatter(),
             FormatterType.TABLE => new TableOutputFormatter(),
             FormatterType.TEXT => new TextOutputFormatter(),
-            _ => throw new NotSupportedException(),
+            FormatterType.NONE => new NoneOutputFormatter(),
+            _ => throw new ArgumentOutOfRangeException(nameof(formatterType), formatterType, INVALID_FORMATTER_ERROR),
         };
     }
 
@@ -23,7 +26,7 @@ public sealed class OutputFormatterFactory : IOutputFormatterFactory
         var success = Enum.TryParse(format, true, out FormatterType type);
         if (!success)
         {
-            throw new NotSupportedException();
+            throw new ArgumentOutOfRangeException(nameof(format), format, INVALID_FORMATTER_ERROR);
         }
         return GetFormatter(type);
     }

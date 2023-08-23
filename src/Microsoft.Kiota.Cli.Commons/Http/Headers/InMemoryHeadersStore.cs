1d7e7282707e0bfa5ff336e3def2e30bfa7feecf
Caleb Kiage <747955+calebkiage@users.noreply.github.com>
2023-08-23 16:44:19 +0300

    Use single InMemoryHeadersStore class with parameter for thread safety.

--- src/Microsoft.Kiota.Cli.Commons/Http/Headers/InMemoryHeadersStore.cs
+++ src/Microsoft.Kiota.Cli.Commons/Http/Headers/InMemoryHeadersStore.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,17 +8,28 @@ namespace Microsoft.Kiota.Cli.Commons.Http.Headers;
 /// <summary>
 /// An in memory headers store.
 /// </summary>
+/// 
 /// <remarks>
-/// This class is not thread safe.
+/// <para>
+/// Use <see cref="Instance"/> static property to get a default thread unsafe
+/// header store and <see cref="ConcurrentInstance"/> static property to get a
+/// thread-safe variant.
+/// </para>
+/// <para>
+/// Use the <see cref="IsConcurrent"/> instance property to determine if an
+/// instance of this class is thread-safe.
+/// </para>
 /// </remarks>
 public sealed class InMemoryHeadersStore : BaseHeadersStore
 {
-    private readonly Dictionary<string, ICollection<string>> _headers = new(StringComparer.OrdinalIgnoreCase);
+    private readonly IDictionary<string, ICollection<string>> _headers;
+    private readonly bool _isConcurrent;
 
     /// <summary>
-    /// Returns a single instance of the <see cref="InMemoryHeadersStore"/>
-    /// singleton.
+    /// Returns a thread unsafe single instance of the
+    /// <see cref="InMemoryHeadersStore"/> singleton.
     /// </summary>
+    /// 
     /// <remarks>
     /// For cases where you might not want to use a single instance across your
     /// application e.g. when using an instance per thread, create a new
@@ -25,6 +37,18 @@ public sealed class InMemoryHeadersStore : BaseHeadersStore
     /// the instance when you need to access the stored headers.
     /// </remarks>
     public static InMemoryHeadersStore Instance { get; } = new();
+
+    /// <summary>
+    /// Returns a thread safe single instance of the
+    /// <see cref="InMemoryHeadersStore"/> singleton.
+    /// </summary>
+    public static InMemoryHeadersStore ConcurrentInstance { get; } = new(true);
+
+    /// <summary>
+    /// A boolean indicating whether the store can be used in multi-threaded
+    /// contexts safely.
+    /// </summary>
+    public bool IsConcurrent => _isConcurrent;
 
     /// <inheritdoc />
     public override IEnumerable<KeyValuePair<string, ICollection<string>>> GetHeaders() => _headers.AsEnumerable();
@@ -49,7 +73,25 @@ public sealed class InMemoryHeadersStore : BaseHeadersStore
     /// <summary>
     /// Creates a new instance of <see cref="InMemoryHeadersStore"/>
     /// </summary>
-    public InMemoryHeadersStore()
+    /// 
+    /// <param name="concurrent">Whether to opt into thread safety.</param>
+    /// 
+    /// <remarks>
+    /// When using the headers store from multiple threads, you can opt into
+    /// thread synchronization by setting <paramref name="concurrent"/> to
+    /// true. This option uses a thread safe backing store, but will incur
+    /// some synchronization overhead.
+    /// </remarks>
+    public InMemoryHeadersStore(bool concurrent = false)
     {
+        if (concurrent)
+        {
+            _isConcurrent = true;
+            _headers = new ConcurrentDictionary<string, ICollection<string>>(StringComparer.OrdinalIgnoreCase);
+        }
+        else
+        {
+            _headers = new Dictionary<string, ICollection<string>>(StringComparer.OrdinalIgnoreCase);
+        }
     }
 }
